e513a088eca99c35ff1b681eb159dee0dd2f67f2
IlyaGubarev <ilya.gubarev@gmail.com>
2013-09-03 05:29:27 +0400

    adt.impl package merged with adt

--- Java/Algorithms/src/main/java/com/ilyagubarev/algorithms/adt/SingleLinkedNodeIterator.java
+++ Java/Algorithms/src/main/java/com/ilyagubarev/algorithms/adt/SingleLinkedNodeIterator.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.ilyagubarev.algorithms.adt.impl;
+package com.ilyagubarev.algorithms.adt;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
