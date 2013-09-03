97d5ae674ce85043129fc9732fa93c0b10787c70
IlyaGubarev <ilya.gubarev@gmail.com>
2013-09-03 05:51:31 +0400

    ListNode added

---
+++ Java/Algorithms/src/main/java/com/ilyagubarev/algorithms/adt/ListNode.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2013 Ilya Gubarev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ilyagubarev.algorithms.adt;
+
+import java.io.Serializable;
+
+/**
+ * Linked list node interface.
+ *
+ * @see Serializable
+ *
+ * @version 1.01, 03 September 2013
+ * @since 03 September 2013
+ * @author Ilya Gubarev
+ */
+public interface ListNode<E> extends Serializable {
+
+    /**
+     * Gets a containing item.
+     *
+     * @return a contained item.
+     */
+    E getItem();
+
+    /**
+     * Gets a reference to the next node.
+     *
+     * @return the next node.
+     */
+    ListNode<E> getNext();
+
+    /**
+     * Sets a new containing item.
+     *
+     * @param item an item to be contained.
+     */
+    void setItem(E item);
+
+    /**
+     * Sets a reference to the next node.
+     *
+     * @param node the next node.
+     */
+    void setNext(ListNode<E> node);
+}
