25c751dba8dd33ff45c14e2f9598b2d0b096c1bc
IlyaGubarev <ilya.gubarev@gmail.com>
2013-09-06 07:01:32 +0400

    SortingHelper added

---
+++ Java/Algorithms/src/main/java/com/ilyagubarev/algorithms/sorting/tools/SortingHelper.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2013 Ilya Gubarev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ilyagubarev.algorithms.sorting.tools;
+
+/**
+ * Sorting service tools.
+ *
+ * @version 1.01, 06 September 2013
+ * @since 06 September 2013
+ * @author Ilya Gubarev
+ */
+public final class SortingHelper {
+
+    /**
+     * Checks if one comparable object is less than another.
+     *
+     * @param subject an object to be tested.
+     * @param sample a sample object.
+     * @return true if the subject is less than the sample.
+     * @throws IllegalArgumentException if the objects are incomparable.
+     */
+    public static boolean isLess(Comparable subject, Comparable sample) {
+        return subject.compareTo(sample) < 0;
+    }
+
+    private SortingHelper() {
+
+    }
+}
