e7f3b28a040b91a138f31e8d9cddee7688783422
IlyaGubarev <ilya.gubarev@gmail.com>
2013-09-12 02:47:29 +0400

    GapProvider added

---
+++ Java/Algorithms/src/main/java/com/ilyagubarev/algorithms/sorting/utils/gapping/GapProvider.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2013 Ilya Gubarev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ilyagubarev.algorithms.sorting.utils.gapping;
+
+import java.util.List;
+
+/**
+ * Gap value provider for D. Shell method sorting algorithm.
+ *
+ * @version 1.01, 12 September 2013
+ * @since 12 September 2013
+ * @author Ilya Gubarev
+ */
+public abstract class GapProvider {
+
+    private int _current;
+    private boolean _inited;
+    private List<Integer> _gaps;
+
+    protected GapProvider() {
+
+    }
+
+    /**
+     * Gets the next gap value.
+     *
+     * @return gap value.
+     */
+    public final int getNext() {
+        if (!_inited) {
+            throw new IllegalStateException("provider is not inited");
+        }
+        if (_current == _gaps.size()) {
+            throw new IllegalStateException("out of gaps");
+        }
+        return _gaps.get(_current++);
+    }
+
+    /**
+     * Resets the provider.
+     *
+     * @param n total amount of items to be sorted.
+     * @throws IllegalArgumentException if total amount is not positive.
+     * @throws IllegalStateException if no gaps are generated.
+     */
+    public final void reset(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("total amount is not positive");
+        }
+        List<Integer> gaps = getGaps(n);
+        if (gaps.size() < 1) {
+            throw new IllegalStateException("no gaps are generated");
+        }
+        _gaps = gaps;
+        _current = 0;
+        _inited = true;
+    }
+
+    /**
+     * Gets a list of values to be used as gaps (f.e. "N/2", "N/4", ..., "1").
+     *
+     * @param n total amount of items to be sorted.
+     * @return a list of gap values.
+     */
+    protected abstract List<Integer> getGaps(int n);
+}
