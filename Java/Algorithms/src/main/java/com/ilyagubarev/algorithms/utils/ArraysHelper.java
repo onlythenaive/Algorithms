187a7bd9ffdd84d5f487a3fc76ebe0c041194ea3
Ilya Gubarev <ilya.gubarev@gmail.com>
2013-09-11 19:37:22 +0400

    ArraysHelper added

---
+++ Java/Algorithms/src/main/java/com/ilyagubarev/algorithms/utils/ArraysHelper.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013 Ilya Gubarev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ilyagubarev.algorithms.utils;
+
+import java.util.Random;
+
+/**
+ * Arrays service utilites.
+ *
+ * @version 1.01, 11 September 2013
+ * @since 11 September 2013
+ * @author Ilya Gubarev
+ */
+public final class ArraysHelper {
+
+    /**
+     * Populates specified array with random items.
+     *
+     * @param array an array to be populated.
+     */
+    public static void populate(Integer[] array) {
+        Random random = new Random();
+        for (int i = 0; i < array.length; ++i) {
+            array[i] = random.nextInt();
+        }
+    }
+
+    private ArraysHelper() {
+
+    }
+}
