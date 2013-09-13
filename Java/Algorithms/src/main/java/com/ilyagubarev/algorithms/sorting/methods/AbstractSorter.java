c3ae53d94ca28d66a63e6869ee0dbe8d3f902a50
IlyaGubarev <ilya.gubarev@gmail.com>
2013-09-13 23:40:12 +0400

    AbstractSorter implemented

--- Java/Algorithms/src/main/java/com/ilyagubarev/algorithms/sorting/methods/AbstractSorter.java
+++ Java/Algorithms/src/main/java/com/ilyagubarev/algorithms/sorting/methods/AbstractSorter.java
@@ -15,99 +15,71 @@
  */
 package com.ilyagubarev.algorithms.sorting.methods;
 
+import com.ilyagubarev.algorithms.adt.Item;
 import com.ilyagubarev.algorithms.adt.ItemArray;
-import com.ilyagubarev.algorithms.adt.ItemArrayFactory;
 import com.ilyagubarev.algorithms.adt.ItemHelper;
-import com.ilyagubarev.algorithms.adt.ItemNodeFactory;
-import com.ilyagubarev.algorithms.adt.tools.Stopwatch;
 
 /**
  * Sorting algorithm common implementation.
  *
- * @version 1.03, 13 September 2013
+ * @see Sorter
+ *
+ * @version 1.04, 13 September 2013
  * @since 07 September 2013
  * @author Ilya Gubarev
  */
-public abstract class AbstractSorter {
-
-    private final ItemHelper _helper;
-    private final ItemArrayFactory _arrayFactory;
-    private final ItemNodeFactory _nodeFactory;
-    private final Stopwatch _stopwatch;
-
-    protected AbstractSorter(ItemHelper helper, ItemArrayFactory arrayFactory,
-            ItemNodeFactory nodeFactory, Stopwatch stopwatch) {
-        
-        _helper = helper;
-        _arrayFactory = arrayFactory;
-        _nodeFactory = nodeFactory;
-        _stopwatch = stopwatch;
-    }
+public abstract class AbstractSorter implements Sorter {
 
     /**
-     * Template sequence of sorting actions.
+     * Checks if the first item is less than the second.
      *
-     * @param target an array to be sorted.
-     *
-     * @see ItemArray
-     */
-    public final void sort(ItemArray target) {
-        prepare(target.getSize(), _arrayFactory, _nodeFactory);
-        _stopwatch.start();
-        method(target, _helper, _arrayFactory, _nodeFactory);
-        _stopwatch.stop();
-        post();
-    }
-
-    /**
-     * Concrete sorting method implementation.
-     *
-     * @param target an array to be sorted.
-     * @param helper an item utility.
-     * @param arrayFactory item arrays provider.
-     * @param nodeFactory item nodes provider.
+     * @param target target array of items.
+     * @param first an index of the first item.
+     * @param second an index of the second item.
+     * @param helper item utility method provider.
+     * @return true if the first item is less than the second.
      *
      * @see ItemArray
-     * @see ItemArrayFactory
      * @see ItemHelper
-     * @see ItemNodeFactory
      */
-    protected abstract void method(ItemArray target, ItemHelper helper,
-            ItemArrayFactory arrayFactory, ItemNodeFactory nodeFactory);
-
-    /**
-     * Actions before the sorting is started.
-     *
-     * @param n target array size.
-     * @param arrayFactory item arrays provider.
-     * @param nodeFactory item nodes provider.
-     * 
-     * @see ItemArrayFactory
-     * @see ItemNodeFactory
-     */
-    protected void prepare(int n, ItemArrayFactory arrayFactory,
-            ItemNodeFactory nodeFactory) {
-
+    protected final boolean less(ItemArray target, int first, int second,
+            ItemHelper helper) {
+        return helper.less(target.read(first), target.read(second));
     }
 
     /**
-     * Actions after the sorting is completed.
+     * Exchanges items of specified indeces with each other.
+     *
+     * @param target target array of items.
+     * @param first an index of the first item.
+     * @param second an index of the second item.
+     *
+     * @see ItemArray
      */
-    protected void post() {
-
+    protected final void swap(ItemArray target, int first, int second) {
+        Item buffer = target.read(first);
+        target.write(first, target.read(second));
+        target.write(second, buffer);
     }
 
     /**
-     * 
+     * Exchanges items with each other if the first is less than the second.
      *
-     * @param target
-     * @param first
-     * @param second
-     * @param helper 
+     * @param target target array of items.
+     * @param first an index of the first item.
+     * @param second an index of the second item.
+     * @param helper item utility method provider.
      *
-     * @see 
+     * @see ItemArray
+     * @see ItemHelper
      */
-    protected final void swap(ItemArray target, int first, int second, ItemHelper helper) {
-        
+    protected final void swapIfLess(ItemArray target, int first, int second,
+            ItemHelper helper) {
+        Item item1 = target.read(first);
+        Item item2 = target.read(second);
+        if (helper.less(item1, item2)) {
+            target.write(first, item2);
+            target.write(second, item1);
+        }
     }
 }
