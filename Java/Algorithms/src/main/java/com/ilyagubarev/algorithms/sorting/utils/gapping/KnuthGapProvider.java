e9b4eb24e097bda9d85c4b2b4b23fae44b2b581b
IlyaGubarev <ilya.gubarev@gmail.com>
2013-09-13 23:57:53 +0400

    KnuthGapProvider added

--- Java/Algorithms/src/main/java/com/ilyagubarev/algorithms/sorting/utils/gapping/KnuthGapProvider.java
+++ Java/Algorithms/src/main/java/com/ilyagubarev/algorithms/sorting/utils/gapping/KnuthGapProvider.java
@@ -14,37 +14,35 @@
  * limitations under the License.
  */
 package com.ilyagubarev.algorithms.sorting.utils.gapping;
-//
-//import java.util.LinkedList;
-//import java.util.List;
-//
-//import com.ilyagubarev.algorithms.adt.collections.ItemsStack;
-//import com.ilyagubarev.algorithms.adt.collections.SimpleLinkedStack;
-//
-///**
-// * Gap value provider based on Donald Knuth sequence.
-// *
-// * @see GapProvider
-// *
-// * @version 1.01, 12 September 2013
-// * @since 12 September 2013
-// * @author Ilya Gubarev
-// */
-//public final class KnuthGapProvider extends GapProvider {
-//
-//    @Override
-//    protected List<Integer> getSequence(int n) {
-//        ItemsStack<Integer> reversed = new SimpleLinkedStack<Integer>();
-//        int gap = 1;
-//        int k = 1;
-//        do {
-//            reversed.push(gap);
-//            gap = ((int) Math.pow(3, ++k) - 1) / 2;
-//        } while (gap <= n / 3);
-//        List<Integer> result = new LinkedList<Integer>();
-//        while (!reversed.isEmpty()) {
-//            result.add(reversed.pop());
-//        }
-//        return result;
-//    }
-//}
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Gap value provider based on Donald Knuth sequence.
+ *
+ * @see GapProvider
+ *
+ * @version 1.01, 12 September 2013
+ * @since 12 September 2013
+ * @author Ilya Gubarev
+ */
+public final class KnuthGapProvider extends GapProvider {
+
+    @Override
+    protected List<Integer> getSequence(int n) {
+        Stack<Integer> reversed = new Stack<Integer>();
+        int gap = 1;
+        int k = 1;
+        do {
+            reversed.push(gap);
+            gap = ((int) Math.pow(3, ++k) - 1) / 2;
+        } while (gap <= n / 3);
+        List<Integer> result = new LinkedList<Integer>();
+        while (!reversed.isEmpty()) {
+            result.add(reversed.pop());
+        }
+        return result;
+    }
+}
