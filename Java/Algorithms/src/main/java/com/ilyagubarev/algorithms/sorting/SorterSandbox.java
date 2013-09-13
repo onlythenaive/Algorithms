b2e49bd1d650f1e2359d20e9d588e4f05741e494
IlyaGubarev <ilya.gubarev@gmail.com>
2013-09-13 23:04:50 +0400

    SorterSandbox added

---
+++ Java/Algorithms/src/main/java/com/ilyagubarev/algorithms/sorting/SorterSandbox.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2013 Ilya Gubarev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ilyagubarev.algorithms.sorting;
+
+import com.ilyagubarev.algorithms.adt.ItemArray;
+import com.ilyagubarev.algorithms.adt.ItemArrayFactory;
+import com.ilyagubarev.algorithms.adt.ItemNodeFactory;
+
+/**
+ *
+ * @author Ilya Gubarev
+ */
+public final class SorterSandbox {
+
+    void run(ItemArray target, ItemArrayFactory arrayFactory, ItemNodeFactory nodeFactory) {
+        
+    }
+}
