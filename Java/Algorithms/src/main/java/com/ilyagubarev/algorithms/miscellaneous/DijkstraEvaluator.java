65b89b0220a719e5df05b8dddcf66d3722c0a35e
Ilya Gubarev <ilya.gubarev@gmail.com>
2013-09-13 20:12:43 +0400

    DijkstraEvaluator repaired

--- Java/Algorithms/src/main/java/com/ilyagubarev/algorithms/miscellaneous/DijkstraEvaluator.java
+++ Java/Algorithms/src/main/java/com/ilyagubarev/algorithms/miscellaneous/DijkstraEvaluator.java
@@ -14,72 +14,71 @@
  * limitations under the License.
  */
 package com.ilyagubarev.algorithms.miscellaneous;
-//
-//import java.io.IOException;
-//import java.io.Reader;
-//
-//import com.ilyagubarev.algorithms.adt.collections.ItemsStack;
-//// import com.ilyagubarev.algorithms.adt.collections.SimpleLinkedStack;
-//import com.ilyagubarev.algorithms.utils.SourceTokenizer;
-//
-///**
-// * Simple arithmetic expression evaluator.
-// * Based on E. Dijkstra "double stack" algorithm. The expression must be
-// * fully braced and each bracet, operand or operator must be spaced from
-// * another, for example: "( ( 1 + 2.5 ) / 4 )". Only four basic
-// * operators ("+", "-", "*", "/") are allowed. 
-// *
-// * @version 1.02, 11 September 2013
-// * @since September 2013
-// * @author Ilya Gubarev.
-// */
-//public final class DijkstraEvaluator {
-//
-//    /**
-//     * Evaluates the result of a source expression.
-//     *
-//     * @param expression a source expression.
-//     * @return expression evaluation result.
-//     * @throws IOException if error occured while reading the expression.
-//     * @throws RuntimeException if the expression is malformed.
-//     */
-//    public static double eval(Reader expression) throws IOException {
-//        ItemsStack<Double> operands = new SimpleLinkedStack<Double>();
-//        ItemsStack<String> operators = new SimpleLinkedStack<String>();
-//        SourceTokenizer tokenizer = SourceTokenizer.create(expression, ' ');
-//        while (!tokenizer.isDepleted()) {
-//            String token = tokenizer.getNext();
-//            if (token.equals(")")) {
-//                double result;
-//                String operator = operators.pop();
-//                double op2 = operands.pop();
-//                double op1 = operands.pop();
-//                if (operator.equals("+")) {
-//                    result = op1 + op2;
-//                } else if (operator.equals("-")) {
-//                    result = op1 - op2;
-//                } else if (operator.equals("*")) {
-//                    result = op1 * op2;
-//                } else if (operator.equals("/")) {
-//                    result = op1 / op2;
-//                } else {
-//                    throw new RuntimeException("unsupported operator");
-//                }
-//                operands.push(result);
-//            } else if ((token.equals("+")) || (token.equals("-"))
-//                    || (token.equals("*")) || (token.equals("/"))) {
-//                operators.push(token);
-//            } else if (!token.equals("(")) {
-//                operands.push(Double.valueOf(token));
-//            }
-//        }
-//        if (!operators.isEmpty() || (operands.getSize() > 1)) {
-//            throw new RuntimeException("expression evaluating error");
-//        }
-//        return operands.pop();
-//    }
-//
-//    private DijkstraEvaluator() {
-//
-//    }
-//}
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Stack;
+
+import com.ilyagubarev.algorithms.utils.SourceTokenizer;
+
+/**
+ * Simple arithmetic expression evaluator.
+ * Based on E. Dijkstra "double stack" algorithm. The expression must be
+ * fully braced and each bracet, operand or operator must be spaced from
+ * another, for example: "( ( 1 + 2.5 ) / 4 )". Only four basic
+ * operators ("+", "-", "*", "/") are allowed. 
+ *
+ * @version 1.03, 13 September 2013
+ * @since September 2013
+ * @author Ilya Gubarev.
+ */
+public final class DijkstraEvaluator {
+
+    /**
+     * Evaluates the result of a source expression.
+     *
+     * @param expression a source expression.
+     * @return expression evaluation result.
+     * @throws IOException if error occured while reading the expression.
+     * @throws RuntimeException if the expression is malformed.
+     */
+    public static double eval(Reader expression) throws IOException {
+        Stack<Double> operands = new Stack<Double>();
+        Stack<String> operators = new Stack<String>();
+        SourceTokenizer tokenizer = SourceTokenizer.create(expression, ' ');
+        while (!tokenizer.isDepleted()) {
+            String token = tokenizer.getNext();
+            if (token.equals(")")) {
+                double result;
+                String operator = operators.pop();
+                double op2 = operands.pop();
+                double op1 = operands.pop();
+                if (operator.equals("+")) {
+                    result = op1 + op2;
+                } else if (operator.equals("-")) {
+                    result = op1 - op2;
+                } else if (operator.equals("*")) {
+                    result = op1 * op2;
+                } else if (operator.equals("/")) {
+                    result = op1 / op2;
+                } else {
+                    throw new RuntimeException("unsupported operator");
+                }
+                operands.push(result);
+            } else if ((token.equals("+")) || (token.equals("-"))
+                    || (token.equals("*")) || (token.equals("/"))) {
+                operators.push(token);
+            } else if (!token.equals("(")) {
+                operands.push(Double.valueOf(token));
+            }
+        }
+        if (!operators.isEmpty() || (operands.size() > 1)) {
+            throw new RuntimeException("expression evaluating error");
+        }
+        return operands.pop();
+    }
+
+    private DijkstraEvaluator() {
+
+    }
+}
