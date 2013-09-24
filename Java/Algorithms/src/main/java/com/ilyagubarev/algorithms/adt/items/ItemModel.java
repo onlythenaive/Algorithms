6bbf51581c24542426c8176ed087e7f80c5224a9
Ilya Gubarev <ilya.gubarev@gmail.com>
2013-09-24 17:11:59 +0400

    adt.items added

--- Java/Algorithms/src/main/java/com/ilyagubarev/algorithms/adt/items/ItemModel.java
+++ Java/Algorithms/src/main/java/com/ilyagubarev/algorithms/adt/items/ItemModel.java
@@ -13,8 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.ilyagubarev.algorithms.adt;
+package com.ilyagubarev.algorithms.adt.items;
 
+import com.ilyagubarev.algorithms.adt.Model;
 import com.ilyagubarev.algorithms.adt.utils.Counter;
 
 /**
